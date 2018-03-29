a0637cfdcea0bbbbfb30f549e8170b27c8a30706
1103-20 <1103-20@1103-20-PC>
2018-03-29 21:35:26 +0900

    followInfo1

--- src/controller/follow/FollowInfoController.java
+++ src/controller/follow/FollowInfoController.java
@@ -1,12 +1,17 @@
 package controller.follow;
 
+import java.util.HashMap;
 import java.util.Map;
+
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import service.follow.FollowService;
 
@@ -17,10 +22,36 @@ public class FollowInfoController {
 	FollowService followService;
 
 	@RequestMapping("followinfo")
-	public String followInfoHandle(Model model, @RequestParam String id) {
-		Map map = followService.memberInfo(id);
+	public String followInfoHandle(Model model, @RequestParam String id, HttpSession session) {
+		Map map = followService.memberInfo((String) session.getAttribute("logon"), id);
 		model.addAttribute("member", map);
 		model.addAttribute("body", "/WEB-INF/view/followInfo.jsp");
 		return "index";
+	}
+
+	@RequestMapping(path = "follow", method = RequestMethod.GET, produces = "application/json;charset=utf-8")
+	@ResponseBody
+	public String followHandle(HttpSession session, @RequestParam String friend) {
+		Map map = new HashMap<>();
+		map.put("id", (String) session.getAttribute("logon"));
+		map.put("friend", friend);
+		if (followService.follow(map)) {
+			return "true";
+		} else {
+			return "false";
+		}
+	}
+
+	@RequestMapping(path = "followcancle", method = RequestMethod.GET, produces = "application/json;charset=utf-8")
+	@ResponseBody
+	public String followCancleHandle(HttpSession session, @RequestParam String friend) {
+		Map map = new HashMap<>();
+		map.put("id", (String) session.getAttribute("logon"));
+		map.put("friend", friend);
+		if (followService.followCancle(map)) {
+			return "true";
+		} else {
+			return "false";
+		}
 	}
 }
--- src/service/follow/FollowService.java
+++ src/service/follow/FollowService.java
@@ -1,5 +1,6 @@
 package service.follow;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.mybatis.spring.SqlSessionTemplate;
@@ -12,7 +13,27 @@ public class FollowService {
 	@Autowired
 	SqlSessionTemplate template;
 
-	public Map memberInfo(String id) {
-		return template.selectOne("member.selectOne", id);
+	public Map memberInfo(String id, String friend) {
+		Map map = template.selectOne("member.selectOne", friend);
+		Map m = new HashMap<>();
+		if (id != null) {
+			m.put("id", id);
+			m.put("friend", friend);
+			// 팔로우인지?
+			if (template.selectOne("follow.selectOne", m) != null) {
+				map.put("FRIEND", true);
+			}
+		} else {
+			map.put("NOTSESSION", true);
+		}
+		return map;
+	}
+
+	public boolean follow(Map map) {
+		return template.insert("follow.insertOne", map) == 1;
+	}
+
+	public boolean followCancle(Map map) {
+		return template.insert("follow.deleteOne", map) == 1;
 	}
 }
