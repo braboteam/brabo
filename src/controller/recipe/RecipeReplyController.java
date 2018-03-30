1c74770dc5bbd15a5af692ab583f34a8af4b9bc7
jooyeon <jooyeon@200.200.200.14>
2018-03-30 21:39:01 +0900

    [레시피] 관리자 페이지 작업중...

--- src/controller/recipe/RecipeReplyController.java
+++ src/controller/recipe/RecipeReplyController.java
@@ -9,12 +9,14 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.google.gson.Gson;
 
+import service.recipe.RecipeDetailService;
 import service.recipe.RecipeReplyService;
 
 @Controller
@@ -24,6 +26,8 @@ public class RecipeReplyController {
 	Gson gson;
 	@Autowired
 	RecipeReplyService rReplyService;
+	@Autowired
+	RecipeDetailService rDetailService;
 
 	
 	@RequestMapping(path="/replyInput",produces="application/json;charset=utf-8")
@@ -59,9 +63,8 @@ public class RecipeReplyController {
 		String id = (String)session.getAttribute("logon");
 		
 		try {
-			list = rReplyService.getReply(ino);
-				list.add(rReplyService.getAvg(ino));
-				list.add(rReplyService.getProfile(id));
+			list = rReplyService.getReplyAjax(ino,id);
+				
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -69,6 +72,41 @@ public class RecipeReplyController {
 		return gson.toJson(list);
 	}
 	
+	//댓글 삭제 처리용 컨트롤러 
+	@RequestMapping("/replyDel")
+	public String replyDelHandle(@RequestParam String rno,Model model) {
+		
+		// rno로 recipe_info 의 pk 값 탐색.
+		String no = rReplyService.getNo(rno);
+		// 댓글 삭제
+		boolean rst = rReplyService.delReply(rno);
+		
+		// 이 아래부터 댓글 삭제 후 리턴 시, 페이지 출력 위한 데이터 셋팅 작업들
+		Map info = rDetailService.getInfo(no);
+		List<Map> detail = rDetailService.getDetail(no);
+		
+		String id = (String)info.get("ID");
+		Map profile = rDetailService.getProfile(id);
+		
+		String items = (String)info.get("ITEMS");
+		String[] itemAr = items.split("#");
+			info.put("ITEMS", itemAr);
+		
+		// 요리 완성된 사진들 담아주기
+		List<Map> fphoto = rDetailService.getFinal(no);	
+		// 댓글 출력 위한 작업.
+		List<Map<String,Object>> reply = rReplyService.getReply(no);	
+		
+		model.addAttribute("profile", profile);
+		model.addAttribute("info", info);
+		model.addAttribute("detail", detail);
+		model.addAttribute("fphoto", fphoto);
+		model.addAttribute("reply",reply);
+		model.addAttribute("rate",rReplyService.getAvg(no));
+		model.addAttribute("body", "/WEB-INF/view/recipeDetail.jsp");
+		
+		return "index";
+	}
 	
 	
 	
